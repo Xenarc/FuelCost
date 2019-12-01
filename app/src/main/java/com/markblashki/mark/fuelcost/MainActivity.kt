b13d46c58441718fd965850e0abad07114af5319
Mark Blashki <markblashki1@gmail.com>
2019-12-01 20:50:33 +1100

    Small Design changes & added Functionality for buttons The update fuel button now just sets the variables

--- app/src/main/java/com/markblashki/mark/fuelcost/MainActivity.kt
+++ app/src/main/java/com/markblashki/mark/fuelcost/MainActivity.kt
@@ -10,10 +10,16 @@ import android.widget.TextView
 import com.google.android.gms.location.FusedLocationProviderClient
 import android.content.Intent
 import android.provider.Settings
+import android.R.attr.button
+import android.widget.Button
+import java.lang.Exception
 
 
 class MainActivity : Activity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var CostofFuel = 0.0
+    private var Economy = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,9 +27,14 @@ class MainActivity : Activity() {
         val viewCostOfFuel = findViewById<View>(R.id.txtCostOfFuel) as EditText
         val viewEconomy = findViewById<View>(R.id.txtEconomy) as EditText
         val viewCostOutput = findViewById<View>(R.id.dispCost) as TextView
+        val viewFuelOutput = findViewById<View>(R.id.dispFuel) as TextView
+        val viewHistoryOutput = findViewById<View>(R.id.dispHistory) as TextView
 
-        val CostofFuel = viewCostOfFuel.text.toString()
-        val Economy = viewEconomy.text.toString()
+        val btnNewTrip = findViewById<View>(R.id.btnNewTrip) as Button
+        val btnUpdateFuel = findViewById<View>(R.id.btnUpdateFuel) as Button
+
+
+        // Set up Location Stuff
         val service = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val enabled = service
             .isProviderEnabled(LocationManager.GPS_PROVIDER)
@@ -34,6 +45,28 @@ class MainActivity : Activity() {
         if (!enabled) {
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
+        }
+
+        // Set OnClick Listeners for the buttons
+        btnNewTrip.setOnClickListener {
+            // New Trip Code
+
+        }
+
+        btnNewTrip.setOnClickListener {
+            // Update Fuel Details
+
+            CostofFuel = try {
+                viewCostOfFuel.text.toString().toDouble()
+            }catch (e: NumberFormatException){
+                0.0
+            }
+
+            Economy = try{
+                viewEconomy.text.toString().toDouble()
+            }catch (e: NumberFormatException){
+                0.0
+            }
         }
     }
 }
