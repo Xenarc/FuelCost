7c8a3c469d13d61bfef46af6ccb8326b2032f6ca
Mark Blashki <markblashki1@gmail.com>
2019-12-04 17:24:53 +1100

    Location permissions now working as intended

--- app/src/main/java/com/markblashki/mark/fuelcost/MainActivity.kt
+++ app/src/main/java/com/markblashki/mark/fuelcost/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.markblashki.mark.fuelcost
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.location.LocationManager
 import android.app.Activity
@@ -9,30 +8,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
-import android.content.Intent
-import android.provider.Settings
 import android.content.pm.PackageManager
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.location.Criteria
-import android.location.Criteria.*
 import android.location.Location
 import android.location.LocationListener
 import android.widget.Toast
-import java.lang.Exception
-import android.content.DialogInterface
-import android.R.string.ok
 
 
 
 
 class MainActivity : Activity() {
+    // Internal Calculation Variables
     private var CostofFuel = 0.0
     private var Economy = 0.0
     private val POSITION_CODE = 1
 
+    // Views
     private lateinit var viewCostOfFuel: EditText
     private lateinit var viewEconomy: EditText
     private lateinit var viewCostOutput:TextView
@@ -41,6 +35,8 @@ class MainActivity : Activity() {
     private lateinit var btnNewTrip: Button
     private lateinit var btnUpdateFuel: Button
 
+    // Location variables
+    private var locationPermitted = false
     private lateinit var locationManager: LocationManager
     private lateinit var provider: String
     private lateinit var currentLocation: Location
@@ -50,37 +46,67 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Sets the view variables from the UI
         initialiseViews()
 
+        // Check if permissions are enabled
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        // Set up Location Stuff
-        if(checkLocationPermission()) {
-            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            provider = locationManager.getProvider(LocationManager.GPS_PROVIDER).toString()
+            // Check if we Should show a rationale for Location Permissions? (Only called once... ever)
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(this)
+                    .setTitle("Location Request")
+                    .setMessage("Location Features Must Be Enabled For This App To Operate")
+                    .setPositiveButton(android.R.string.ok) { dialogInterface, i ->
+                        //Prompt the user once explanation has been shown
+                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), POSITION_CODE)
+                    }
+                    .create()
+                    .show()
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), POSITION_CODE)
+            }
+        } // Permissions allowed v
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            initLocationObjects()
+
+            //Request location updates:
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                750,
+                10.0f, locationListenerGPS)
         }
 
         // Set OnClick Listeners for the buttons
-        btnNewTrip.setOnClickListener {
-            // New Trip Code
+        btnUpdateFuel.setOnClickListener {btnUpdateFuelClicked()}
+        btnNewTrip.setOnClickListener {btnNewTripClicked()}
+    }
 
-            // Test GPS
-            doGPS()
+    private fun btnUpdateFuelClicked() {
+        // New Trip Code
+        doGPS()
+    }
+
+    private fun btnNewTripClicked() {
+        // Update Fuel Details
+
+        CostofFuel = try {
+            viewCostOfFuel.text.toString().toDouble()
+        }catch (e: NumberFormatException){
+            0.0
         }
 
-        btnNewTrip.setOnClickListener {
-            // Update Fuel Details
-
-            CostofFuel = try {
-                viewCostOfFuel.text.toString().toDouble()
-            }catch (e: NumberFormatException){
-                0.0
-            }
-
-            Economy = try{
-                viewEconomy.text.toString().toDouble()
-            }catch (e: NumberFormatException){
-                0.0
-            }
+        Economy = try{
+            viewEconomy.text.toString().toDouble()
+        }catch (e: NumberFormatException){
+            0.0
         }
     }
 
@@ -122,97 +148,37 @@ class MainActivity : Activity() {
         btnUpdateFuel = findViewById<View>(R.id.btnUpdateFuel) as Button
     }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-private fun checkLocationPermission(): Boolean {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                AlertDialog.Builder(this)
-                    .setTitle("Location Request")
-                    .setMessage("Location Features Must Be Enabled For This App To Operate")
-                    .setPositiveButton(android.R.string.ok) { dialogInterface, i ->
-                        //Prompt the user once explanation has been shown
-                        ActivityCompat.requestPermissions(
-                            this@MainActivity,
-                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                            POSITION_CODE
-                        )
-                    }
-                    .create()
-                    .show()
-
-
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    POSITION_CODE
-                )
-            }
-            return false
-        } else {
-            return true
-        }
-    }
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             POSITION_CODE -> {
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(
-                            this,
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        ) == PackageManager.PERMISSION_GRANTED
-                    ) {
-
-                        //Request location updates:
-                        locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            750,
-                            10.0f, locationListenerGPS)
-                    }
-
-                } else {
-                    // permission denied, die
+                    initLocationObjects()
+                }else{
                     invokeFailedLocationDialogue()
                 }
-
-                return
             }
         }
     }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private fun initLocationObjects() {
+        if (ContextCompat.checkSelfPermission( applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission( applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // No permissions, die
+            invokeFailedLocationDialogue()
+        }else {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            provider = locationManager.getProvider(LocationManager.GPS_PROVIDER).toString()
+        }
+    }
+
     private fun doGPS() {
-
-        // Test Location
-        checkLocationPermission()
-
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION),
-            POSITION_CODE
-        )
-
-        currentLocation = locationManager.getLastKnownLocation(provider)
-
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            currentLocation = locationManager.getLastKnownLocation(provider)
+        }
         val toast = Toast.makeText(
             applicationContext,
             currentLocation.latitude.toString(),
@@ -244,12 +210,14 @@ private fun checkLocationPermission(): Boolean {
     }
 
     private fun invokeFailedLocationDialogue() {
+        print("Kind Kill --------------------------------------------------------------------------------------")
         val alertDialogBuilder = AlertDialog.Builder(this)
 
         alertDialogBuilder.setTitle("Location Not Enabled")
         alertDialogBuilder.setMessage("Sorry, this app requires location services")
         alertDialogBuilder.setPositiveButton(android.R.string.ok) { _,_ ->
             finishAffinity()
+
         }
 
         val alertDialog = alertDialogBuilder.create()
